57fd33e7f42650a146e424901de56617f8618392
Dell <Dell@DESKTOP-H7KG118>
2022-11-17 20:01:34 +0530

    first java project in git pls check

---
+++ checkgit_javaproject/src/checkgit_javaproject/testrepo_hello.java
@@ -0,0 +1,12 @@
+package checkgit_javaproject;
+
+public class testrepo_hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello git this is my project");
+
+	}
+
+}
---
+++ checkgit_javaproject/src/module-info.java
@@ -0,0 +1,2 @@
+module checkgit_javaproject {
+}
